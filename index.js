191be64bf9fb7f6cf9d3a0f0fe6bfec4fe805572
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-03-14 11:47:35 -0500

    ob.md

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+
+    if (left > 0) { 
+        dodger.style.left = `${left - 10}px`; 
+    }
+}
+console.log(`Before moving right, position: ${dodger.style.left}`);
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+    const gameWidth = document.getElementById('game').clientWidth;
+    const dodgerWidth = dodger.offsetWidth;
+
+    if (left + dodgerWidth + 10 <= gameWidth) {
+        dodger.style.left = `${left + 10}px`;
+    }
+}
